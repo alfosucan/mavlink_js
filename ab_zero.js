1e80abf07385fa7915364266e3070e02c5f0db90
Alfonso Osuna <acosuna.ext@catec.aero>
2021-02-23 15:08:29 +0100

    Request info and request message type implemented

--- ab_zero.js
+++ ab_zero.js
@@ -1,10 +1,19 @@
 let {mavlink10, MAVLink10Processor} = require('./mavlink_v1/mavlink')
 var SerialPort = require('serialport')
+var command_Q = new Array().fill(-1);
+var DEBUG = true;
 
+log_to_console = function (...args) {
+    if (DEBUG) {
+        console.log(...args);
+    }
+}
 class MavLink {
+
     constructor(ssyd, component_id, port) {
         this.ssyd = ssyd;
         this.component_id = component_id;
+
         try {
             this.serial = new SerialPort(port, {
                 baudRate: 921600,
@@ -21,24 +30,94 @@ class MavLink {
         this.serial.on('data', raw_buffer => {
             this.mav.parseBuffer(raw_buffer);
         });
+        this.subscribe('COMMAND_ACK', this.commandACKCallback)
+
     }
 
     sendCommand(command, ...params) {
+        // avoid send until is already been processed
+        let index = command_Q.find(val => val==command);
+        if (index > -1) {
+            log_to_console(`Message ID: ${command} already sent`)
+            return;
+        }
+
+        command_Q.push(command);
         var command_msg = new mavlink10.messages.command_long(
             2,
             1,
             command,
-            1,
+            101,
             ...params
         );
         var buff = new Buffer.from(command_msg.pack(this.mav));
         this.serial.write(buff);
     }
+
+    commandACKCallback(ack_msg) {
+
+        // remove message from send Q
+        let index = command_Q.find(val => val==ack_msg.command);
+        if (index > -1) {
+            log_to_console(`Removed ${ack_msg.command} at position ${index}`);
+            command_Q.splice(index, 1);
+        }
+
+        switch (ack_msg.result) {
+            case mavlink10.MAV_RESULT_TEMPORARILY_REJECTED:
+                throw Error("MAV_RESULT_TEMPORARILY_REJECTED");
+            case mavlink10.MAV_RESULT_DENIED:
+                throw Error("MAV_RESULT_DENIED");
+            case mavlink10.MAV_RESULT_UNSUPPORTED:
+                throw Error("MAV_RESULT_UNSUPPORTED");
+            case mavlink10.MAV_RESULT_FAILED:
+                throw Error("MAV_RESULT_FAILED");
+            case mavlink10.MAV_RESULT_IN_PROGRESS:
+                throw Error("MAV_RESULT_IN_PROGRESS");
+            case mavlink10.MAV_RESULT_CANCELLED:
+                throw Error("MAV_RESULT_CANCELLED");
+            default:
+                break;
+        }
+     }
+
+    requestMessage(mav_msg) {
+        this.sendCommand(mavlink10.MAV_CMD_REQUEST_MESSAGE,
+            mav_msg, 0,0,0,0,0,1)
+    }
+
+    async getInfo(msg_name, mav_msg) {
+        let promise = new Promise((resolve, reject) => {
+
+            // subscribe to msg type
+            this.subscribe(msg_name, (msg) => {
+                let info_obj = new Object();
+
+                this.requestMessage(mav_msg); // request msg
+
+                info_obj.name = msg_name
+                msg.fieldnames.forEach(field => {
+                    info_obj[field] = msg[field];
+                })
+                resolve(info_obj);
+            })
+
+            setTimeout(() => {
+                reject(new Error(`Cannot get info for ${msg_name} : ${mav_msg}`))
+            }, 1000);
+
+        })
+        let info = await promise
+        this.mav.removeAllListeners(msg_name)
+        return info;
+    }
+
     subscribe(event, callback) {
         this.mav.on(event, callback);
     }
+
     unsubscribe(event) {
-        this.mav.off(event);
+        this.mav.removeAllListeners(event);
     }
 }
 
--- main.js
+++ main.js
@@ -1,69 +1,35 @@
-/**
- * Ejemplo de como funciona esta wea
- */
 
-// var {mavlink10, MAVLink10Processor} = require('./mavlink_v1/mavlink')
-// var SerialPort = require('serialport')
-// let serial_port = new SerialPort('/dev/ttyACM0', {
-//     baudRate: 921600,
-//     autoOpen: true,
-//     parity: 'none',
-//     dataBits: 8,
-//     stopBits: 1
-// })
-// let mav1 = new MAVLink10Processor(null, 1, 1)
-// mav1.file = serial_port;
-
-// var test_command_long = new mavlink10.messages.command_long();
-// test_command_long.param1 = mavlink10.MAVLINK_MSG_ID_RAW_IMU;
-// test_command_long.command = mavlink10.MAV_CMD_GET_MESSAGE_INTERVAL;
-// test_command_long.target_system = 2;
-// test_command_long.target_component = 1;
-// test_command_long.confirmation = 101;
-// var t = new Buffer.from(test_command_long.pack(mav1));
-// serial_port.on('data', function (msg) {
-
-//     mav1.parseBuffer(msg)
-// })
-// mav1.on('COMMAND_ACK', function (msg) {
-//     console.log(msg)
-// })
-// mav1.on('MESSAGE_INTERVAL', function (msg) {
-//     var s = `ID: ${msg.message_id}\t INTERVAL: ${msg.interval_us}`
-//     console.log(s)
-// })
-
-// setInterval(() => {
-//     serial_port.write(t)
-// }, 1500)
 
 var Mavlink = require('./ab_zero')
-let {mav01, MAVLink10Processor} = require('./mavlink_v1/mavlink')
+let {mavlink10, MAVLink10Processor} = require('./mavlink_v1/mavlink')
 
 mav = new Mavlink(255, 1, '/dev/ttyACM0');
 
-function log_mesg_interval (msg) {
-    var s = `ID: ${msg.message_id}\tINTERVAL: ${msg.interval_us}`;
-    console.log(s);
-}
-mav.subscribe('MESSAGE_INTERVAL', log_mesg_interval)
-
-mav.subscribe('COMMAND_ACK', msg => {
-    var s = `COMMAND: ${msg.command}\tRESULT: ${msg.result}`;
-    console.log(s)
+mav.subscribe('HOME_POSITION', msg => {
+    console.log(msg)
 })
 
-mav.subscribe("ATTITUDE", msg => {
-    var s = `ROLL: ${msg.roll}\tPITCH: ${msg.pitch}\tYAW ${msg.yaw}`;
-    console.log(s)
-})
+// mav.subscribe('ATTITUDE', msg => {
+//     console.log(msg)
+// })
 
 setInterval(() => {
-    mav.sendCommand(mav01.MAV_CMD_SET_MESSAGE_INTERVAL,
-        mav01.MAVLINK_MSG_ID_ATTITUDE, 1000000, 0)
-}, 1000)
+    mav.requestMessage(mavlink10.MAVLINK_MSG_ID_HOME_POSITION)
+}, 5000)
 
 setInterval(() => {
-    mav.sendCommand(mav01.MAV_CMD_GET_MESSAGE_INTERVAL,
-        mav01.MAVLINK_MSG_ID_ATTITUDE)
-}, 1500)
+    mav.sendCommand(mavlink10.MAV_CMD_SET_MESSAGE_INTERVAL,
+        mavlink10.MAVLINK_MSG_ID_ATTITUDE, 1000000, 0)
+}, 5000)
+
+
+//get gps info
+mav.getInfo('GPS_RAW_INT', mavlink10.MAVLINK_MSG_ID_GPS_RAW_INT).then(
+    gps => console.log(gps),
+    error => console.log(error)
+)
+
+mav.getInfo('THIS_IS_NOT_A_MSG', mavlink10.I_DONT_EXIST).then(
+    gps => console.log(gps),
+    error => console.log(error)
+)
